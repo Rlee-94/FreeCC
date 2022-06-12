f81cb78e533f2bf4344b1ea17026b67348b2fed8
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-06-11 19:51:08 -0500

    Added new exercise files

---
+++ Functional_programming/Pass Arguments to Avoid External Dependence in a Function.js
@@ -0,0 +1,12 @@
+//Let's update the incrementer function to clearly declare its dependencies.
+
+//Write the incrementer function so it takes an argument, and then returns a result after increasing the value by one.
+
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(value) {
+    let result = value += 1
+    return result
+    // Only change code above this line
+}
